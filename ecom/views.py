5e85953f136b9e8a6648f6d305fc2dff2cc9c556
Nguyen Van Tri <vantri1010@gmail.com>
2024-08-17 01:47:31 +0700

    play with HTML Form and Django Forms

--- ecom/views.py
+++ ecom/views.py
@@ -1,5 +1,34 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .forms import ContactForm
+
 def home_page(request):
-    return render(request, "home_page.html", {})
+    context = {
+        "title": "Hello World!",
+        "content": "Welcome to the homepage"
+    }
+    return render(request, "home_page.html", context)
+
+def about_page(request):
+    context = {
+        "about": "About page",
+        "content": "Welcome to the about page"
+    }
+    return render(request, "home_page.html", context)
+
+def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
+    context = {
+        "title": "Contact",
+        "content": "Welcome to the contact page",
+        "form": contact_form
+    }
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+    # if request.method == "POST":
+    #     # print(request.POST)
+    #     print(request.POST.get('fullname'))
+    #     print(request.POST.get('email'))
+    #     print(request.POST.get('content'))
+    return render(request, "contact/view.html", context)
