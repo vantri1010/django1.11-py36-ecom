f9a14e1362be71d3a6c97636b29f989f3885b07c
Nguyen Van Tri <vantri1010@gmail.com>
2024-08-25 03:46:53 +0700

    3. Include Tag 4. Pass Arguments with Include 5. Reusable List View Snippets 6. Reverse for URLs 7. Navbar

--- src/products/models.py
+++ src/products/models.py
@@ -1,5 +1,6 @@
 import random
 import os
+from django.urls import reverse
 from django.db import models
 from django.db.models.signals import pre_save, post_save
 from .utils import unique_slug_generator
@@ -58,8 +59,8 @@ class Product(models.Model):
     objects = ProductManager()
 
     def get_absolute_url(self):
-        return "/products/{slug}/".format(slug=self.slug)
-        # return reverse("products:detail", kwargs={"slug": self.slug})
+        # return "/products/{slug}/".format(slug=self.slug)
+        return reverse("products:detail", kwargs={"slug": self.slug})
 
     def __str__(self):
         return self.title
