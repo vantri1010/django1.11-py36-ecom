111212663dff4d3f8b3176f62ece9d68667a6c31
Nguyen Van Tri <vantri1010@gmail.com>
2024-09-01 02:25:31 +0700

    8. M2M Changed Signal to Calculate Cart Total

--- src/carts/views.py
+++ src/carts/views.py
@@ -2,16 +2,5 @@ from django.shortcuts import render
 from .models import Cart
 
 def cart_home(request):
-    cart_id = request.session.get("cart_id", None)
-    qs = Cart.objects.filter(id=cart_id)
-    if qs.count() == 1:
-        print('Cart ID exists with user :')
-        cart_obj = qs.first()
-        print(cart_obj.user)
-        if request.user.is_authenticated() and cart_obj.user is None:
-            cart_obj.user = request.user
-            cart_obj.save()
-    else:
-        cart_obj = Cart.objects.new(user=request.user)
-        request.session['cart_id'] = cart_obj.id
-    return render(request, "carts/home.html", {})
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
+    return render(request, "carts/home.html", {"cart": cart_obj})
