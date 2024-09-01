3b449e874f9850bafc7aa7e2c14fede7802e70c5
Nguyen Van Tri <vantri1010@gmail.com>
2024-09-02 03:52:17 +0700

    7. Cart Component

--- src/carts/urls.py
+++ src/carts/urls.py
@@ -2,14 +2,10 @@ from django.conf.urls import url
 
 from .views import (
         cart_home, 
-        # cart_update, 
-        # checkout_home,
-        # checkout_done_view
+        cart_update, 
         )
 
 urlpatterns = [
     url(r'^$', cart_home, name='home'),
-    # url(r'^checkout/success/$', checkout_done_view, name='success'),
-    # url(r'^checkout/$', checkout_home, name='checkout'),
-    # url(r'^update/$', cart_update, name='update'),
+    url(r'^update/$', cart_update, name='update'),
 ]
--- src/products/views.py
+++ src/products/views.py
@@ -2,6 +2,7 @@ from django.http import Http404
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404
 
+from carts.models import Cart
 from .models import Product
 
 class ProductFeaturedListView(ListView):
@@ -37,7 +38,6 @@ def product_list_view(request):
     context = {
         'object_list' : queryset
     }
-
     return render(request, "products/list.html", context)
 
 class ProductDetailSlugView(DetailView):
@@ -46,30 +46,34 @@ class ProductDetailSlugView(DetailView):
 
     def get_context_data(self, *args, **kwargs):
         context = super(ProductDetailSlugView, self).get_context_data(*args, **kwargs)
-        # cart_obj, new_obj = Cart.objects.new_or_get(self.request)
-        # context['cart'] = cart_obj
+        cart_obj, new_obj = Cart.objects.new_or_get(self.request)
+        context['cart'] = cart_obj
         return context
 
     def get_object(self, *args, **kwargs):
         request = self.request
         slug = self.kwargs.get('slug')
-
         #instance = get_object_or_404(Product, slug=slug, active=True)
         try:
             instance = Product.objects.get(slug=slug, active=True)
         except Product.DoesNotExist:
             raise Http404("Not found..")
+        except Product.MultipleObjectsReturned:
+            qs = Product.objects.filter(slug=slug, active=True)
+            instance = qs.first()
+        except:
+            raise Http404("Uhhmmm ")
         return instance
 
 class ProductDetailView(DetailView):
-    # queryset = Product.objects.all()
+    #queryset = Product.objects.all()
     template_name = "products/detail.html"
 
     def get_context_data(self, *args, **kwargs):
         context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
         print(context)
         return context
-    
+
     def get_object(self, *args, **kwargs):
         request = self.request
         pk = self.kwargs.get('pk')
@@ -82,9 +86,7 @@ def product_detail_view(request, pk=None, *args, **kwargs):
     instance = Product.objects.get_by_id(pk)
     if instance is None:
         raise Http404("Product doesn't exist")
-    
     context = {
         'object' : instance
     }
-
     return render(request, "products/detail.html", context)
