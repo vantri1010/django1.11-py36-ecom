ed8dae1a073bba5d008488b2acb940587902f210
Nguyen Van Tri <vantri1010@gmail.com>
2024-09-12 05:17:47 +0700

    19. Reuse Addresses for Checkout 20. Checkout Success

--- src/carts/views.py
+++ src/carts/views.py
@@ -3,9 +3,10 @@ from django.shortcuts import render, redirect
 from accounts.forms import LoginForm, GuestForm
 from accounts.models import GuestEmail
 from addresses.forms import AddressForm
+from addresses.models import Address
+from billing.models import BillingProfile
 from orders.models import Order
 from products.models import Product
-from billing.models import BillingProfile
 from .models import Cart
 
 def cart_home(request):
@@ -42,7 +43,10 @@ def checkout_home(request):
     shipping_address_id = request.session.get("shipping_address_id", None)
 
     billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
+    address_qs = None
     if billing_profile is not None:
+        if request.user.is_authenticated():
+            address_qs = Address.objects.filter(billing_profile=billing_profile)
         order_obj, order_obj_created = Order.objects.new_or_get(billing_profile, cart_obj)
         if shipping_address_id:
             order_obj.shipping_address = Address.objects.get(id=shipping_address_id)
@@ -52,13 +56,24 @@ def checkout_home(request):
             del request.session["billing_address_id"]
         if billing_address_id or shipping_address_id:
             order_obj.save()
-    
+
+    if request.method == "POST":
+        "check that order is done"
+        is_done = order_obj.check_done()
+        if is_done:
+            order_obj.mark_paid()
+            request.session['cart_items'] = 0
+            del request.session['cart_id']
+            return redirect("cart:success")
     context = {
-        "title": "Login",
         "object": order_obj,
         "billing_profile": billing_profile,
         "login_form": login_form,
         "guest_form": guest_form,
         "address_form": address_form,
+        "address_qs": address_qs,
     }
     return render(request, "carts/checkout.html", context)
+
+def checkout_done_view(request):
+    return render(request, "carts/checkout-done.html", {})
