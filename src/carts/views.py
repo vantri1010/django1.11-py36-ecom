ca1c92bd56672e5df20d4e37067305b265f74f54
Nguyen Van Tri <vantri1010@gmail.com>
2024-09-16 04:28:43 +0700

    10. Products & Async using Ajax

--- src/carts/views.py
+++ src/carts/views.py
@@ -1,13 +1,28 @@
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 
 from accounts.forms import LoginForm, GuestForm
 from accounts.models import GuestEmail
+
 from addresses.forms import AddressForm
 from addresses.models import Address
+
 from billing.models import BillingProfile
 from orders.models import Order
 from products.models import Product
 from .models import Cart
+
+def cart_detail_api_view(request):
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
+    products = [{
+            "id": x.id,
+            "url": x.get_absolute_url(),
+            "name": x.title, 
+            "price": x.price
+            } 
+            for x in cart_obj.products.all()]
+    cart_data  = {"products": products, "subtotal": cart_obj.subtotal, "total": cart_obj.total}
+    return JsonResponse(cart_data)
 
 def cart_home(request):
     cart_obj, new_obj = Cart.objects.new_or_get(request)
@@ -24,10 +39,21 @@ def cart_update(request):
         cart_obj, new_obj = Cart.objects.new_or_get(request)
         if product_obj in cart_obj.products.all():
             cart_obj.products.remove(product_obj)
+            added = False
         else:
             cart_obj.products.add(product_obj) # cart_obj.products.add(product_id)
+            added = True
         request.session['cart_items'] = cart_obj.products.count()
         # return redirect(product_obj.get_absolute_url())
+        if request.is_ajax(): # Asynchronous JavaScript And XML / JSON
+            print("Ajax request")
+            json_data = {
+                "added": added,
+                "removed": not added,
+                "cartItemCount": cart_obj.products.count()
+            }
+            return JsonResponse(json_data, status=200) # HttpResponse
+            # return JsonResponse({"message": "Error 400"}, status=400) # Django Rest Framework
     return redirect("cart:home")
 
 def checkout_home(request):
--- src/ecom/urls.py
+++ src/ecom/urls.py
@@ -23,6 +23,7 @@ from django.contrib.auth.views import LogoutView
 
 from accounts.views import login_page, register_page, guest_register_view
 from addresses.views import checkout_address_create_view, checkout_address_reuse_view
+from carts.views import cart_detail_api_view
 from .views import home_page, about_page, contact_page
 
 urlpatterns = [
@@ -40,6 +41,7 @@ urlpatterns = [
     url(r'^products/', include("products.urls", namespace='products')),
     url(r'^search/', include("search.urls", namespace='search')),
     url(r'^cart/', include("carts.urls", namespace='cart')),
+    url(r'^api/cart/$', cart_detail_api_view, name='api-cart'),
 ]
 
 if settings.DEBUG:
